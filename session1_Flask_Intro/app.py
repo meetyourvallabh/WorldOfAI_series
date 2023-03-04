5fd2d9c6ef0a895748a3a1c1273491ce46863d0c
Vallabh Hake <vallabh@Vallabhs-MacBook-Pro.local>
2023-03-04 11:36:25 +0530

    Flask sesison 2, signup page

--- session1_Flask_Intro/app.py
+++ session1_Flask_Intro/app.py
@@ -1,11 +1,32 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/login")
 def login():
+    # bussiness logic
     return render_template("login.html")
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    # Bussiness logic
+    if request.method == "POST":
+        print("its a post call")
+        print(request.form["first_name"])
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        password = request.form["password"]
+        phone_number = request.form["phone_number"]
+        city = request.form["city"]
+
+        print(first_name, email, password)
+        return render_template("signup.html")
+
+    print("its an get call")
+    return render_template("signup.html")
 
 
 @app.route("/bye")
