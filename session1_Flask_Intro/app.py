9d035f6c7195f87a59291090d43d063a7c443eb3
Vallabh Hake <vallabh@Vallabhs-MacBook-Pro.local>
2023-03-09 22:13:37 +0530

    Flask sesison 2.2, Flask authentication, login page

--- session1_Flask_Intro/app.py
+++ session1_Flask_Intro/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_pymongo import PyMongo
 from flask_bcrypt import Bcrypt
 
@@ -9,9 +9,44 @@ mongo = PyMongo(app)
 bcrypt = Bcrypt(app)
 
 
-@app.route("/login")
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/dashboard", methods=["GET", "POST"])
+def dashboard():
+    return render_template("dashboard.html")
+
+
+@app.route("/login", methods=["GET", "POST"])
 def login():
     # bussiness logic
+    if request.method == "POST":
+        # accept data from frontend(form data) - > email and password
+        email = request.form["email"]
+        password = request.form["password"]
+
+        # Check if that email is present in database or not
+        found_user = mongo.db.users.find_one({"email": email})
+        # print(found_user)
+        if found_user:
+            print("user found")
+            # if email is present then compare password_hash from db with user's entered password
+            is_password_matched = bcrypt.check_password_hash(
+                found_user["password"], password
+            )
+
+            if is_password_matched:
+                print("correct password")
+                # if password also matched then login successfull and redirect to dashboard
+                return redirect("/dashboard")
+            else:
+                print("incorrect password")
+
+        else:
+            print("no user found")
+
     return render_template("login.html")
 
 
